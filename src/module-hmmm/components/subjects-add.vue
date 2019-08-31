bb5adfa59ce60ad5c75e6e1734473b93355a5667
LJL <l_moondream@163.com>
2019-08-31 17:41:05 +0800

    修改学科功能完成，优化添加删除学科功能

--- src/module-hmmm/components/subjects-add.vue
+++ src/module-hmmm/components/subjects-add.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <!-- 新建学科弹出框 -->
       <el-form ref="formData" :rules="rules" :model="formData" label-width="100px">
         <el-form-item prop="subjectName" label="学科名称">
           <el-input v-model="formData.subjectName" style="width:500px;" placeholder="请输入"></el-input>
@@ -12,7 +11,7 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="close">取消</el-button>
-        <el-button type="primary" @click="addSubject(formData)">确认</el-button>
+        <el-button type="primary" @click="edit.id ? editSubject(formData):addSubject(formData)">确认</el-button>
       </div>
     </div>
   </div>
@@ -20,22 +19,45 @@
 
 <script>
 // 引入学科添加
-import { add } from '@/api/hmmm/subjects'
+import { add, update } from '@/api/hmmm/subjects'
 export default {
   name: 'SubjectsAdd',
+  props: ['edit'],
   data() {
     return {
       dialogTableVisible: true,
       formData: {
         subjectName: '',
-        isFrontDisplay: false
+        isFrontDisplay: false,
+        id: null
       },
+      // 校验规则
       rules: {
         subjectName: [{ required: true, message: '学科名称不能为空' }]
       }
     }
   },
   methods: {
+    // 修改
+    editSubject(formData) {
+      this.$refs.formData.validate(async isOk => {
+        if (isOk) {
+          let result = await update(formData)
+          if (result.status === 200) {
+            this.$message({ message: '修改成功', type: 'success' })
+            this.close()
+          } else {
+            this.$message({ message: result.statusText, type: 'warning' })
+          }
+        }
+      })
+    },
+    // 编辑时获取当前修改学科的信息
+    getAddSubject() {
+      this.formData.subjectName = this.edit.subjectName
+      this.formData.isFrontDisplay = !!this.edit.isFrontDisplay
+      this.formData.id = this.edit.id
+    },
     // 关闭弹窗
     close() {
       return this.$emit('closedialog', false)
@@ -47,12 +69,15 @@ export default {
           let result = await add(formData)
           // 添加成功清空输入框内容，并关闭弹窗
           if (result.status === 200) {
-            this.$refs.formData.resetFields()
+            this.$message({ message: '添加成功', type: 'success' })
             this.close()
           }
         }
       })
     }
+  },
+  created() {
+    this.getAddSubject()
   }
 }
 </script>
--- src/module-hmmm/pages/subjects.vue
+++ src/module-hmmm/pages/subjects.vue
@@ -3,7 +3,7 @@
     <div class="app-container">
       <el-card class="manage-content">
         <el-row class="add-btn" style="margin-bottom:30px;">
-          <el-button @click="dialogTableVisible = true">新增学科</el-button>
+          <el-button @click="addItem">新增学科</el-button>
         </el-row>
         <!-- 搜索按钮区域 -->
         <el-form label-width="80px" class="manage-search">
@@ -31,7 +31,7 @@
             <template slot-scope="scoped">
               <el-link type="primary">学科分类</el-link>
               <el-link type="primary">学科标签</el-link>
-              <el-link type="primary">修改</el-link>
+              <el-link @click="editItem(scoped.row)" type="primary">修改</el-link>
               <el-link @click="delItem(scoped.row)" type="primary">删除</el-link>
             </template>
           </el-table-column>
@@ -49,10 +49,12 @@
             :page-size="page.pageSize"
           ></el-pagination>
         </el-row>
-        <el-dialog title="新增学科" :visible.sync="dialogTableVisible">
-          <!-- 新增学科组件 -->
-          <subjects-add @closedialog="closeDialog"></subjects-add>
-        </el-dialog>
+        <template v-if="dialogTableVisible">
+          <el-dialog title="新增学科" :visible.sync="dialogTableVisible">
+            <!-- 新增学科组件 -->
+            <subjects-add :edit="editInfo" @closedialog="closeDialog"></subjects-add>
+          </el-dialog>
+        </template>
       </el-card>
     </div>
   </div>
@@ -61,8 +63,8 @@
 <script>
 // 引入新增学科组件
 import subjectsAdd from '@/module-hmmm/components/subjects-add'
-// 引入学科列表,学科删除
-import { list, remove } from '@/api/hmmm/subjects'
+// 引入学科列表,学科删除,学科修改
+import { list, remove, update } from '@/api/hmmm/subjects'
 export default {
   name: 'SubjectsList',
   data() {
@@ -77,17 +79,32 @@ export default {
         currentPage: 1,
         pageSize: 10,
         total: 0
-      }
+      },
+      editInfo: {}
     }
   },
   methods: {
+    // 学科新增
+    addItem() {
+      this.dialogTableVisible = true
+      this.editInfo = {}
+    },
+    // 学科修改
+    editItem(row) {
+      this.dialogTableVisible = true
+      this.editInfo = row
+    },
     // 删除学科分类
     delItem(row) {
-       this.$confirm('确认删除此条分类？', '提示').then(async() => {
-         await remove(row)
+      this.$confirm('确认删除此条分类？', '提示').then(async () => {
+        let result = await remove(row)
+        if (result.status === 200) {
+          this.$message({message: '删除成功', type: 'success'})
+          this.getArticlesList()
+          }
         //  刷新列表
-        this.getArticlesList()
-       })
+        
+      })
     },
     // 关闭弹出框
     closeDialog(close) {
