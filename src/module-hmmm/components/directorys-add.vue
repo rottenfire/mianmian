a840690efbd516100315b8a3b264c92b6031b35a
Sun Hai Yong <1497415670@qq.com>
2019-09-01 21:38:46 +0800

    新建功能，修改功能，删除功能，基本成型

--- src/module-hmmm/components/directorys-add.vue
+++ src/module-hmmm/components/directorys-add.vue
@@ -1,17 +1,86 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">目录添加</div>
+    <!-- 输入框 -->
+    <div class="app-container">
+      <span>*目录名称 :</span>
+      <el-input
+        placeholder="请输入内容"
+        v-model="info.directoryName"
+        clearable
+        maxlength="25"
+        class="inputcon"
+      ></el-input>
+    </div>
+    <!-- 按钮 -->
+    <div class="button">
+      <el-button type="primary" @click="repair.id ? edit(info) :confirm(info)">确定</el-button>
+      <el-button @click="cancle">取消</el-button>
+    </div>
   </div>
 </template>
 
 <script>
+// 按需导入目录添加
+import { add, update } from '../../api/hmmm/directorys'
 export default {
+  created() {
+    console.log(this.repair)
+
+    this.getinfo()
+  },
   name: 'DirectorysAdd',
   data() {
-    return {}
-  }
+    return {
+      // input: '',
+      info: {
+        id: null,
+        directoryName: '',
+        subjectID: 1
+      }
+    }
+  },
+  methods: {
+    async confirm(info) {
+      await add(info)
+      await this.$emit('confirm', false, '1')
+      this.info.directoryName = ''
+    },
+    async edit(info) {
+      // console.log(this.info)
+      let res = await update(info)
+      console.log(res)
+      this.$emit('edit', false)
+    },
+    cancle() {
+      this.$emit('cancle', false)
+      this.input = ''
+    },
+    getinfo() {
+      this.info.directoryName = this.repair.directoryName
+      this.info.id = this.repair.id
+      console.log(this.info)
+      
+    }
+  },
+  props: ['repair']
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.dashboard-container {
+  margin-top: 20px;
+  overflow: hidden;
+  .app-container {
+    span {
+      font-weight: 700;
+    }
+    .inputcon {
+      width: 300px;
+    }
+  }
+  .button {
+    float: right;
+    margin-top: 80px;
+  }
+}
 </style>
