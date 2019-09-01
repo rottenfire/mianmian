52a924722f370d88c3983866d59a6d4ce75b330d
rotten_fire <zhangjiaweia@126.com>
2019-09-01 22:40:13 +0800

    修改配置文件格式

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,7 +35,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
@@ -49,7 +49,7 @@ module.exports = {
       {
         // 3) less处理loader配置
         test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"]
+        use: ['style-loader', 'css-loader', 'less-loader']
       },
       {
         test: /\.js$/,
